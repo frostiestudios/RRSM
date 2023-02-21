d20bf656c39dc797fe9f1d529986a988bcd2a120
thememetsar <mattwiner04@gmail.com>
2023-02-20 18:37:33 -0800

    2

--- client.py
+++ client.py
@@ -6,34 +6,15 @@ def receive_message():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("0.0.0.0", 12345))
     s.listen(1)
-
     conn, addr = s.accept()
     print(f"Connection from {addr}")
     data = conn.recv(1024).decode()
     print(f"Received: {data}")
-    app = gui("Message")
-    app.infoBox("Message Received", data)
-    app.addLabel(f"From:{addr}")
+    if data=="CMD":
+        print("Running a command")
     conn.close()
     receive_message()
     
-def receive_file():
-    # Create a socket and listen for incoming connections
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("0.0.0.0", 12345))
-    s.listen(1)
-
-    # Accept a connection
-    conn, addr = s.accept()
-    print(f"Connection from {addr}")
-
-    # Receive and save the file
-    with open("received_file.bin", "wb") as f:
-        f.write(conn.recv(1024))
-
-    # Close the socket
-    conn.close()
 # Create a loop to continuously listen for incoming connections
 while True:
     receive_message()
-    receive_file()
--- main.py
+++ main.py
@@ -6,39 +6,11 @@ with open('clients.json') as f:
 
 ip_addresses = [pc['ip_address'] for pc in data['pcs']]    
 def send_content(btn):
-    # Get the selected options from the app
-    content_type = app.getOptionBox("Content Type")
     ip_address = app.getOptionBox("IP Address")
-
-    # Send the content based on the selected type
-    if content_type == "Link":
-        # Send a link
-        link = app.getEntry("Link")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip_address, 12345))
-        s.sendall(link.encode())
-        s.close()
-    elif content_type == "Command":
-        # Send a command
-        command = app.getEntry("Command")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip_address, 12345))
-        s.sendall(command.encode())
-        s.close()
-    elif content_type == "File":
-        # Send a file
-        file_path = app.getEntry("File")
-        with open(file_path, "rb") as f:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((ip_address, 12345))
-            s.sendfile(f)
-            s.close()
-    elif content_type == "Message":
-        # Send a message
-        message = app.getEntry("Message")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip_address, 12345))
-        s.sendall(message.encode())
+    message = app.getEntry("Message")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip_address, 12345))
+    s.sendall(message.encode())
 def newclient(btn):
     name = app.getEntry("PC NAME")
     ipad = app.getEntry("IP Address")
@@ -51,9 +23,7 @@ def newclient(btn):
 
 app = gui()
 app.addLabelEntry("Message")
-app.addFileEntry("File")
 app.addButton("Send", send_content)
-app.addOptionBox("Content Type",["Message","File","Command","Link"])
 app.addOptionBox("IP Address", ip_addresses)
 
 app.startLabelFrame("Add A New Client")
